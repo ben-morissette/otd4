6ecdc99c3698f21492c0ac9d016d7114d2d37736
ben-morissette <benmorissette@cmail.carleton.ca>
2025-08-27 22:41:02 +0000

    33

--- pages/api/teams.js
+++ pages/api/teams.js
@@ -1,22 +1,37 @@
 import fetch from "node-fetch";
 
+const SPORT_APIS = {
+  NHL: "hockey/nhl",
+  NFL: "football/nfl",
+  NBA: "basketball/nba",
+};
+
 export default async function handler(req, res) {
   const { sport } = req.query;
-  if (!sport) return res.status(400).json({ error: "Missing sport parameter" });
 
-  let url;
-  if (sport === "NHL") url = "https://site.api.espn.com/apis/site/v2/sports/hockey/nhl/teams";
-  else if (sport === "NFL") url = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams";
-  else if (sport === "NBA") url = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams";
-  else return res.status(400).json({ error: "Invalid sport" });
+  if (!sport || !SPORT_APIS[sport]) {
+    return res.status(400).json({ error: "Invalid or missing sport parameter" });
+  }
+
+  const url = `http://site.api.espn.com/apis/site/v2/sports/${SPORT_APIS[sport]}/teams`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
+
     const teams = {};
-    (data.sports?.[0]?.leagues?.[0]?.teams || []).forEach(t => {
-      teams[t.team.abbreviation] = t.team.displayName;
+    const teamsData = data?.sports?.[0]?.leagues?.[0]?.teams || [];
+
+    teamsData.forEach(t => {
+      const abbr = t.team.abbreviation;
+      const name = t.team.displayName;
+      teams[abbr] = name;
     });
+
+    if (Object.keys(teams).length === 0) {
+      return res.status(404).json({ error: "No teams found" });
+    }
+
     res.status(200).json({ teams });
   } catch (err) {
     console.error(err);
